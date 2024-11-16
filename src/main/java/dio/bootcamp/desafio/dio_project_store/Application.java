5a7b84ab327c90c4df0e1b68eeabada7ff3cb179
lizbethla <nayarethlizbethla@gmail.com>
2024-11-16 15:47:51 -0500

    commit inicial

---
+++ src/main/java/dio/bootcamp/desafio/dio_project_store/Application.java
@@ -0,0 +1,13 @@
+package dio.bootcamp.desafio.dio_project_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioProjectStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioProjectStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dio/bootcamp/desafio/dio_project_store/DioProjectStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.bootcamp.desafio.dio_project_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioProjectStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
